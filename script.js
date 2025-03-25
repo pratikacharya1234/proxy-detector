b91a76bf638100d15c5dd27be0e0bb6281bd36bc
pratikacharya1234 <acharyapratik214@gmail.com>
2025-03-25 12:11:21 -0600

    feat: implement loader functionality and restructure backend detection logic

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
 let checkButton = document.getElementById('checkBtn');
 let clearButton = document.getElementById('clearBtn');
-let result = document.getElementById('result');
 
+
+//function for loader to show
+function showLoader() {
+    loader.style.display = 'block';
+}
+
+//function for loader to hide 
+function hideLoader() {
+    loader.style.display = 'none';
+}
+
+
+//function to show the input text is human writtten or ai written
 checkButton.addEventListener('click', function() {
+    showLoader();
     let input = document.getElementById('textarea').value;
     if (input === '') {
         alert('Please enter a number.');
         return;
     }
+    //fetching the data from the api
+    fetch(`${apiUrl}?q=${input}&app_id=86a48720&app_key=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      displayResult(data.hits);
+      hideLoader(); 
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      hideLoader(); 
+      alert("enter correctly")
+    });
 });
 
+//function to clear the input field
 clearButton.addEventListener('click',function(){
     document.getElementById('textarea').value="";
     result.innerHTML="";
     result.style.display = 'none';
 });
+
+
+//function to display the result
+function displayResult(result) {
+    const result = document.getElementById('result');
+    result.innerHTML = '';
+  
+    result.forEach(result => {
+      
+      const resultCard = document.createElement('div');
+      resultCard.classList.add('resultCard');
+  
+      const resultPredict = document.createElement('h2');
+      resultPredict.textContent = label;
+  
+  
+      resultCard.appendChild(resultCard);
+      resultPredict.appendChild(resultPredict);
+
+      resultCard.appendChild(resultCard);
+    });
+  }
+  
+  hideLoader();
